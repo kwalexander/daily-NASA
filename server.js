532091562ae8154faadfbc14991b3a56493f691b
Victor Soto <109629515+Vsoto7697@users.noreply.github.com>
2022-11-29 19:08:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/index.js');
 const sequelize = require('./config/connection');
 
+// import sessions library
+const session = require('express-session');
 const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const path = require('path');
+//middleware for front end
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/homepage', (req, res) => {
+ res.render('event');
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded( {extended: true} ));
 
-// turn on routes
+// session middleware
+const sess = {
+    secret: 'secretMessage',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({db:sequelize})
+};
+
+app.use(session(sess));
+
+//turn on routes last
 app.use(routes);
+
+const exhbs = require('express-handlebars');
+const hbs = exhbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
